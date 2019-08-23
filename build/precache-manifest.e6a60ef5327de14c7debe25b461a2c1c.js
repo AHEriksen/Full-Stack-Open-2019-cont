2cc567b6b55f879102132581ed15aef8807edfe6
ahoer <andersahe@gmail.com>
2019-08-23 12:39:39 +0200

    uibuild

---
+++ build/precache-manifest.e6a60ef5327de14c7debe25b461a2c1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5206fa845897f0fa3426340d8ba9c118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "463e5e7dd1b61b57e269",
+    "url": "/static/css/main.90f81d31.chunk.css"
+  },
+  {
+    "revision": "1f1e64caa1eecb40e664",
+    "url": "/static/js/2.9aeab016.chunk.js"
+  },
+  {
+    "revision": "463e5e7dd1b61b57e269",
+    "url": "/static/js/main.69bacf99.chunk.js"
+  },
+  {
+    "revision": "e42c3f6bb30015257370",
+    "url": "/static/js/runtime~main.632a408e.js"
+  }
+]);
